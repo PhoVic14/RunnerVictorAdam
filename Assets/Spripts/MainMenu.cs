ae71da19fe02d1c6067d45de9ae0d46a3234b75b
Pholien Victor <180229@CCM-Asty-Moulin.local>
2024-02-09 15:58:03 +0100

    Ajout de texture et menu

---
+++ Assets/Spripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+ 
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+}
